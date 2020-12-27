5279113eab0c7ec20dd9372fc82063d839f6e763
YuMiJson <2430284055@qq.com>
2020-12-27 17:29:22 +0800

    登录界面的修改

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,34 +4,61 @@
           <img src="../assets/logo/soushuo.svg" alt="">
           <input type="text"  placeholder="搜索音乐">
       </div>
-         <div class='round'></div>
+      <div class="login">
+        <div class='round'></div>
          <div class='online' @click = "login">
               点击登录 
-         </div>
+         </div> 
+      </div>
                 <div v-show="appear">
                 <div class="cancel"> 
                 <div class="cancel_main" >
-                      <div class = "cancel_img" @click="cancelClick">
+                      <div class = "cancel_img" @click="loginCancel">
                          <img src="../assets/logo/cha.svg" >
                       </div>
                       <div class = "cancel_logo">
                          <img src="../assets/logo/logo.jpg" >
                       </div>  
-                       <div class='login_main'>
+                       <div class='login_main' >
+                            <div class = "Login_way" v-show="login_appear">
+                              <div class="Login_email" @click="Email" >
+                                    <img src="../assets/logo/login-Email.svg" alt="">
+                              </div>
+                              <div class="Login_Phone" @click="Phone">
+                                    <img src="../assets/logo/Phone.svg" alt="">
+                              </div>
+                            </div>
+
+                            <!-- 手机 -->
+                            <div v-show="Phone_appear">
                             <div class='login_image'>
                               <img src="../assets/logo/zhanghao.svg" alt="">
                               <input type="text" >
                             </div>
                             <div class='login_image'>
                                <img src="../assets/logo/yuechi.svg" alt="">
-                               <input type="password">
-                            </div>
+                               <input type="password" >
+                            </div> 
                             <div class='login_footer'>
                               <button>注册</button>
                               <button>登录</button> 
                             </div>
-                            
-    
+                            </div>
+                            <!-- 邮箱 -->
+                            <div v-show="Email_appear">
+                            <div class='login_image'>
+                              <img src="../assets/logo/zhanghao.svg" alt="">
+                              <input type="text" >
+                            </div>
+                            <div class='login_image'>
+                               <img src="../assets/logo/yuechi.svg" alt="">
+                               <input type="password" >
+                            </div> 
+                            <div class='login_footer'>
+                              <button>注册</button>
+                              <button>登录</button> 
+                            </div>
+                            </div>
                        </div>
                 </div>        
                 </div> 
@@ -41,158 +68,67 @@
 </template>
 
 <script>
-// import login from '../views/login.vue'
+import '../assets/css/login.css'
 export default {
      name:'Navigation',
      data(){
        return{
          appear:false,
+         login_appear:true,
+         Phone_appear:false,
+         Email_appear:false,
+        
        }
      },
       components:{
        
       },
      methods:{
+       /**
+        *  
+        */
        login(){
-       this.appear = !this.appear
+       this.appear = !this.appear,
+       this.login_appear=true,
+       this.Phone_appear=false,
+       this.Email_appear=false
        },
-       cancelClick(){
+
+       loginCancel(){
         this.appear = !this.appear
+       },
+       Phone(){
+          this.login_appear = !this.login_appear
+          this.Phone_appear = !this.Phone_appear
+       },
+       Email(){
+         this.login_appear = !this.login_appear
+         this.Email_appear = !this.Email_appear
        }
+
      }
 }
 </script>
 
 <style>
-   .navigation{
-      display:flex;
-      align-items: center;
-      width:100%; 
-      height:100%;
-   }
-   .search{
-     width: 200px;
-     height: 33px;
-     display:flex;
-     align-items: center;
-     margin-left:100px;
-     border-radius:15px;
-     background-color: #e1e1e1;
-   }
-   .search input{
-     background-color: #e1e1e1;
-     border:none;
-     outline-width: 0px;
-   }
-   .search img{
-     width: 20px;
-     height: 20px;
-     padding:5px
-   }
-    .round{
-      width: 30px;
-      height: 30px;
-      background-color:hsl(271, 79%, 82%);
-      border-radius:50%;
-      margin-left: 250px;
-    }
-    .online{
-      font-size: 0.7rem;
-      font-weight: 400;
-      margin-left:5px;
-    }
-    .cancel{
-       width:100%;
-       height:100%; 
-       position:fixed;
-       top: 0px;
-       left: 0px;
-       bottom: 0px;
-       right: 0px;
-       background:rgba(128, 184, 247, 0.2);
-    }  
-    .cancel_main{
-       height:300px;
-       width: 400px;
-       position:absolute; 
-       top: 0px;
-       left: 0px;
-       bottom: 0px;
-       right: 0px;
-       margin: auto;      
-       z-index:-1; 
-       overflow: hidden;   
-       border-radius:0.3em;  
-       box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1) inset,0 .5em 1em rgba(0, 0, 0, 0.3);   
-       text-shadow: 0 1px 1px hsla(0,0%,100%,.3);  
-    }
-    .cancel_main::before{
-       content: '';   
-       position: absolute;   
-       top: 0; 
-       right: 0; 
-       bottom: 0; 
-       left: 0;   
-       margin: -30px;   
-       z-index: -2;   
-       -webkit-filter: blur(20px);   
-       filter: blur(20px);   
-    }
-     .cancel_img img{
-       position: absolute;
-       top: 5px;
-       right: 3px;
-       width: 20px;
-       height: 20px;
-    }
-    .cancel_logo{
-      padding:45px;
+   .Login_way{
       display: flex;
-      justify-content: center;
-    }
-    .cancel_logo img{
-      width: 80px;
-      height: 80px;
-      border-radius:50%;
-
-    }
-    .login_main{
-      display: flex;
-      align-items: center;
-      flex-direction:column;
-    }
-     .login_main input{
-       margin-top:5px;
-       width: 180px;
-       height: 30px;
-       border:none;
-       outline-width: 0px;
-       background-color:#94d0f326;
-     }
-     .login_main img{
-       height:20px;
-       width: 20px;
-       margin-right: 5px;
-     }
-     .login_image{
-       display: flex;
-       align-items: center;
-     }
-     .login_footer button{
-        width:60px;
-        height: 20px;
-        font-size:15px;
-        color:hsl(244, 100%, 74%);
-        margin:10px 0px 0px 10px;
-        border:none;
-        outline-width: 0px;
-        text-transform: uppercase;
-        transition: 0.2s;
-        background-size: 200% auto;
-        background-image: linear-gradient(to right, #a1c4fd 0%, #c2e9fb 51%, #a1c4fd 100%);
-     }
-     .login_footer button:hover{
-       background-position: right center
-     }
+      width: 100%;
+      justify-content: space-around;
+   }
+   .Login_email{
+     width: 50px;
+     height: 50px;
+     background-image:url(../assets/logo/login-Email.svg);
+     background-repeat: no-repeat;
+     background-size:50px 50px ;
+   }
+   .Login_Phone{
+     width: 50px;
+     height: 50px;
+     background-image:url(../assets/logo/Phone.svg);
+     background-repeat: no-repeat;
+     background-size:50px 50px ;
+   }
     
 </style>
